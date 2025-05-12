4a69c3e7be88a2069ba87a0a60f712c6cad9966a
mn-sohel <sohel.m0087@gmail.com>
2025-05-12 23:32:57 +0600

    header topbar right part done
    header top right

---
+++ src/components/header/topbartComponents/TopLeftBar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { BsTelephone } from 'react-icons/bs'
+import { IoLocationOutline } from 'react-icons/io5'
+import { Link } from 'react-router-dom'
+
+const TopLeftBar = () => {
+  return (
+     <div className='flex items-center gap-[50px] relative after:content-[""] after:absolute after:w-[1px] after:h-[32px] after:bg-[#BFBFBF] after:left-[61%] after:top-[50%] after:-translate-y-1/2'>
+        <Link to="https://maps.app.goo.gl/JPxftAdbRVY8srcz8" target='_blank' className='flex items-center gap-2'><IoLocationOutline />123 Main Street, Anytown USA</Link>
+        <Link to="tel:01256952" className='flex items-center gap-2'><BsTelephone />+1 (555) 123-4567</Link>
+    </div>
+)
+}
+
+export default TopLeftBar
