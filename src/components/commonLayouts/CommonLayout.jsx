7fc4ed3576b7cf8963efa8d43368f835e885ca32
mn-sohel <sohel.m0087@gmail.com>
2025-05-14 01:02:24 +0600

    footer topbar part done
    footer top

--- src/components/commonLayouts/CommonLayout.jsx
+++ src/components/commonLayouts/CommonLayout.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Container from './Container'
 import Header from '../header/Header'
+import Footer from '../footer/Footer'
 
 const CommonLayout = () => {
   return (
     <>
         <Header/>
         <Outlet/>
-        <div>Footer</div>
+        <Footer/>
 
     </>
   )
